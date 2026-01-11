15d30f561090b9774b932d7ec86cef9912cd7ab4
husainsteven <husainstefen@gmail.com>
2026-01-11 20:29:37 +0700

    Upload laporan week 2

---
+++ praktikum/week1-setup-hello-pos/src/HelloOOP.java
@@ -0,0 +1,19 @@
+class Produk {
+   String nama;
+   int harga;
+   Produk(String nama, int harga) {
+      this.nama = nama;
+      this.harga = harga;
+   }
+}
+
+public class HelloOOP {
+   public static void main(String[] args) {
+      String nim = "240202836";
+      String namaMhs = "HUSAIN STEFANO";
+      
+      System.out.println("Hello FIRENDS");
+      System.out.println("Nama: " + namaMhs + ", NIM: " + nim);
+      
+   }
+}
